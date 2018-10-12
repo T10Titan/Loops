940ed2276dffe931ab0badc7832b91ec1ce1eb3f
Trujillo <ttru4447@740s-pl-129.local>
2018-10-12 09:21:19 -0600

    Revert "updated userRun"
    This reverts commit 01e3264e827d1c23e8015f49f3232a217e44ddae.

--- src/loopoer/LoopedController.java
+++ src/loopoer/LoopedController.java
@@ -9,4 +9,81 @@ public class LoopedController
 	{
 		loopy();
 	}
+<<<<<<< HEAD
 }
+=======
+	
+	private void loopy()
+	{
+		boolean isDone = false;
+		int count = 0;
+		
+		while (!isDone)
+		{
+			JOptionPane.showMessageDialog(null, "Hey");
+			count++;
+			if(count >= 5)
+			{
+				isDone = true;
+			}
+			
+			
+		}
+		
+		for (int loop = 0; loop < 30; loop += 2 )
+		{
+			JOptionPane.showMessageDialog(null, "the loop value is: " + loop);
+		}
+		
+		
+	}
+	
+	private void askUser()
+	{
+		String response = JOptionPane.showInputDialog(null, "what is the distance");
+		while (!validDouble(response))
+		{
+			response = JOptionPane.showInputDialog(null, "nooooo! type in a valid number for the distance");
+		}
+		Object userRun;
+		userRun.setDistance(Double.parseDouble(response));
+	}
+		public boolean validInt(String maybeInt)
+		{
+			boolean isValid = false;
+					
+			try
+			{
+						Integer.parseInt(maybeInt);
+						isValid = true;
+						
+			}
+			catch(NumberFormatException error)
+			{
+				JOptionPane.showMessageDialog(null, "You type interger value now ");
+			}
+			return isValid;
+			
+			
+		}
+		
+		public boolean validDouble(String maybeDouble)
+		{
+			boolean isValid = false;
+			try 
+			{
+				Double.parseDouble(maybeDouble);
+				isValid = true;
+			}
+			catch(NumberFormatException error)
+			{
+				JOptionPane.showMessageDialog(null, "This requires a double value aka something with a . > )");
+			}
+			return isValid;
+		}
+	
+	
+	
+	
+}
+>>>>>>> parent of 01e3264... updated userRun
